41ca992fa676231ecb2557d8f27bd6148c17955d
Murilo Alves <mbalves1@outlook.com>
2021-01-07 11:17:15 -0300

    inserindo tooltip

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
-import * as teste from './modules/teste.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
-teste.teste1()
-teste.teste2()
-console.log(teste.senha);
-
-console.log(this)
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
+initTooltip();
